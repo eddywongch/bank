e9ab6eff42409c2a84ea050919f516f21440963e
Eddy Wong <eddy.wong@datastax.com>
2022-08-12 13:00:15 -0400

    Modified load with Astra settings/SCB

--- back/load_csv.sh
+++ back/load_csv.sh
@@ -19,6 +19,26 @@
 DSBULK_HOME=/home/automaton/dsbulk/dsbulk-1.7.0/
 DSBULK_EXE=$DSBULK_HOME/bin/dsbulk
 
+# Astra
+#  Dsbulk upload csv
+# /Users/eddy.wong/programs/dsbulk-1.8.0/bin/dsbulk load 
+# -url /Users/eddy.wong/workspace/datastax/astra/bank/Account.csv 
+# -k bank -t "Account" 
+# -b /Users/eddy.wong/keys/secure-connect-ds5-poc.zip 
+# -u DEeyDlsUfdMWrWgDdETwSZtS 
+# -p  pAvGQUZnKoqBdwNtqXm7Ucneu5,ZcvpDrUaXCRvGlHjXY5O0CR9KNdnwD8S8SX8eQ7.1F_xeEW77hjAlHo25ZQIhuBexIfGlD6Q6IlLONr779t+8D_Gjf5EHZprI+5mg 
+# -header true
+
+DSBULK_HOME=/Users/eddy.wong/programs/dsbulk-1.8.0/
+DSBULK_EXE=$DSBULK_HOME/bin/dsbulk
+
+BUNDLE=/Users/eddy.wong/keys/secure-connect-ds5-poc-plus.zip
+#USERNAME=DEeyDlsUfdMWrWgDdETwSZtS
+#PASSWORD=pAvGQUZnKoqBdwNtqXm7Ucneu5,ZcvpDrUaXCRvGlHjXY5O0CR9KNdnwD8S8SX8eQ7.1F_xeEW77hjAlHo25ZQIhuBexIfGlD6Q6IlLONr779t+8D_Gjf5EHZprI+5mg 
+USERNAME=test
+PASSWORD=test
+
+
 # GRAPH is the graph/keyspace to load to
 GRAPH=$1
 
@@ -28,21 +48,24 @@ if [ -z "$GRAPH" ]; then
 	GRAPH=mtb
 fi
 
-$DSBULK_EXE load -g $GRAPH -v Account -url Account.csv -header true
-$DSBULK_EXE load -g $GRAPH -v Party -url Party.csv -header true
-$DSBULK_EXE load -g $GRAPH -e owns -from Party -to Account -url owns.csv -header true
-$DSBULK_EXE load -g $GRAPH -v Company -url Company.csv -header true
-$DSBULK_EXE load -g $GRAPH -e works_for -from Party -to Company -url works_for.csv -header true
+echo "$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v "Account" -url Account.csv  -header true"
 
-$DSBULK_EXE load -g $GRAPH -v Transaction -url Transaction.csv -header true
-$DSBULK_EXE load -g $GRAPH -e withdraws_from -from Transaction -to Account -url withdraws_from.csv -header true
-$DSBULK_EXE load -g $GRAPH -e deposits_to -from Transaction -to Account -url deposits_to.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v "Account" -url Account.csv  -header true
 
-$DSBULK_EXE load -g $GRAPH -v Email -url Email.csv -header true
-$DSBULK_EXE load -g $GRAPH -e uses_email -from Party -to Email -url uses_email.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v Party -url Party.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -e owns -from Party -to Account -url owns.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v Company -url Company.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -e works_for -from Party -to Company -url works_for.csv -header true
 
-$DSBULK_EXE load -g $GRAPH -v Phone -url Phone.csv -header true
-$DSBULK_EXE load -g $GRAPH -e uses_phone -from Party -to Phone -url uses_phone.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Transaction -url Transaction.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e withdraws_from -from Transaction -to Account -url withdraws_from.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e deposits_to -from Transaction -to Account -url deposits_to.csv -header true
 
-$DSBULK_EXE load -g $GRAPH -v Address -url Address.csv -header true
-$DSBULK_EXE load -g $GRAPH -e resides_at -from Party -to Address -url resides_at.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Email -url Email.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e uses_email -from Party -to Email -url uses_email.csv -header true
+
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Phone -url Phone.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e uses_phone -from Party -to Phone -url uses_phone.csv -header true
+
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Address -url Address.csv -header true
+$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e resides_at -from Party -to Address -url resides_at.csv -header true
