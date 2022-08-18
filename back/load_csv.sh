4f3a3b583d418a5de84008abb5446b3af4655b12
Eddy Wong <eddy.wong@datastax.com>
2022-08-18 19:11:08 +0000

    Updated bank to get rid of mtb stuff. Made more generic

--- back/load_csv.sh
+++ back/load_csv.sh
@@ -3,7 +3,8 @@
 #
 # Eddy Wong
 # eddy.wong@datastax.com
-# Mar 24, 2021
+# Created: Mar 24, 2021
+# Updated: Aug 18, 2022
 
 # Loads csv files to a graph/keyspace
 #
@@ -12,12 +13,17 @@
 # load_csv.sh [target graph] 
 #
 # Examples:
-# > load_csv.sh mtb1
+# > load_csv.sh bank
 # 
 # > load_csv.sh
 
-DSBULK_HOME=/home/automaton/dsbulk/dsbulk-1.7.0/
+# Loacation  of DSBulk
+DSBULK_HOME=/home/automaton/dsbulk/dsbulk-1.8.0
 DSBULK_EXE=$DSBULK_HOME/bin/dsbulk
+
+# Hardcoding to my dev server
+HOST=10.101.34.200
+PORT=9042
 
 # Astra
 #  Dsbulk upload csv
@@ -29,9 +35,7 @@ DSBULK_EXE=$DSBULK_HOME/bin/dsbulk
 # -p  pAvGQUZnKoqBdwNtqXm7Ucneu5,ZcvpDrUaXCRvGlHjXY5O0CR9KNdnwD8S8SX8eQ7.1F_xeEW77hjAlHo25ZQIhuBexIfGlD6Q6IlLONr779t+8D_Gjf5EHZprI+5mg 
 # -header true
 
-DSBULK_HOME=/Users/eddy.wong/programs/dsbulk-1.8.0/
-DSBULK_EXE=$DSBULK_HOME/bin/dsbulk
-
+# Astra stuff
 BUNDLE=/Users/eddy.wong/keys/secure-connect-ds5-poc-plus.zip
 #USERNAME=DEeyDlsUfdMWrWgDdETwSZtS
 #PASSWORD=pAvGQUZnKoqBdwNtqXm7Ucneu5,ZcvpDrUaXCRvGlHjXY5O0CR9KNdnwD8S8SX8eQ7.1F_xeEW77hjAlHo25ZQIhuBexIfGlD6Q6IlLONr779t+8D_Gjf5EHZprI+5mg 
@@ -44,28 +48,36 @@ GRAPH=$1
 
 if [ -z "$GRAPH" ]; then
     	#echo "Base dir must be provided"
-	# Setting detault to 'mtb'
+	# Setting detault to 'bank'
 	GRAPH=bank
 fi
 
-echo "$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v "Account" -url Account.csv  -header true"
+# ASTRA Flags like secure bundle. Use for loading into Artemis
+if [ -z "$ASTRA" ]; then
+	SCB_FLAGS=
+else
+	SCB_FLAGS=`echo "-b $BUNDLE -u $USERNAME -p $PASSWORD"`
+fi
 
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v "Account" -url Account.csv  -header true
 
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v Party -url Party.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -e owns -from Party -to Account -url owns.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v Company -url Company.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -e works_for -from Party -to Company -url works_for.csv -header true
+echo "$DSBULK_EXE load -h $HOST -p $PORT $SCB_FLAGS -g $GRAPH -v "Account" -url Account.csv  -header true"
+echo "FLAGS: $SCB_FLAGS"
+$DSBULK_EXE load -h $HOST -p $PORT $SCB_FLAGS -g $GRAPH -v "Account" -url Account.csv  -header true
 
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Transaction -url Transaction.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e withdraws_from -from Transaction -to Account -url withdraws_from.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e deposits_to -from Transaction -to Account -url deposits_to.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v Party -url Party.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -e owns -from Party -to Account -url owns.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -v Company -url Company.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD -g $GRAPH -e works_for -from Party -to Company -url works_for.csv -header true
 
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Email -url Email.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e uses_email -from Party -to Email -url uses_email.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Transaction -url Transaction.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e withdraws_from -from Transaction -to Account -url withdraws_from.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e deposits_to -from Transaction -to Account -url deposits_to.csv -header true
 
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Phone -url Phone.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e uses_phone -from Party -to Phone -url uses_phone.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Email -url Email.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e uses_email -from Party -to Email -url uses_email.csv -header true
 
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Address -url Address.csv -header true
-$DSBULK_EXE load -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e resides_at -from Party -to Address -url resides_at.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Phone -url Phone.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e uses_phone -from Party -to Phone -url uses_phone.csv -header true
+
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -v Address -url Address.csv -header true
+$DSBULK_EXE load -h $HOST -p $PORT -b $BUNDLE -u $USERNAME -p $PASSWORD  -g $GRAPH -e resides_at -from Party -to Address -url resides_at.csv -header true
